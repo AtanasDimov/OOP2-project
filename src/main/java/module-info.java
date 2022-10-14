11e2925fcb41eed525d05aea67a4b3c265135b47
Atanas Dimov <atdimov71@gmail.com>
2022-10-14 13:35:06 +0300

    Initial build commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.librarysoftware {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.librarysoftware to javafx.fxml;
+    exports com.example.librarysoftware;
+}
