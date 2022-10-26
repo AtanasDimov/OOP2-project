8e1e86113da0d1d254944bb62a4330aeefde46f4
Nikola <nikola.lazarov@smart-soft.net>
2022-10-26 17:41:29 +0300

    Added publishDate to BaseLibraryItem

--- src/Library.Dto/java/DTOLibraryItems/BaseLibraryItem.java
+++ src/Library.Dto/java/DTOLibraryItems/BaseLibraryItem.java
@@ -2,6 +2,7 @@ package DTOLibraryItems;
 
 import Contracts.LibraryItemInterface;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 public class BaseLibraryItem implements LibraryItemInterface {
@@ -9,11 +10,13 @@ public class BaseLibraryItem implements LibraryItemInterface {
     private UUID readerID = null;
     private String title;
     private String description;
+    private LocalDate publishDate;
 
-    public BaseLibraryItem(String title, String description) {
+    public BaseLibraryItem(String title, String description, LocalDate publishDate) {
         this.id = UUID.randomUUID();
         this.title = title;
         this.description = description;
+        this.publishDate = publishDate;
     }
 
     public UUID getId() {
@@ -34,5 +37,13 @@ public class BaseLibraryItem implements LibraryItemInterface {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public LocalDate getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(LocalDate publishDate) {
+        this.publishDate = publishDate;
     }
 }
