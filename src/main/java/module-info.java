2608b939a5e327d7655abf9ac629091ff478d4fb
Nikola <nikola.lazarov@smart-soft.net>
2022-11-12 20:25:58 +0200

    Issue with javax.naming

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,12 +3,14 @@ module com.example.librarysoftware {
     requires javafx.fxml;
     requires javafx.web;
 
+
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires java.persistence;
+    requires org.hibernate.orm.core;
 
     opens com.example.librarysoftware to javafx.fxml;
     exports com.example.librarysoftware;
