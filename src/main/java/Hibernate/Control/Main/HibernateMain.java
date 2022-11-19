f1dbcc20bc8fcbde9af0679e69c2eebe2c39765c
Nikola <nikola.lazarov@smart-soft.net>
2022-11-19 14:26:30 +0200

    Added drivers and settings for postgres

--- src/main/java/Hibernate/Control/Main/HibernateMain.java
+++ src/main/java/Hibernate/Control/Main/HibernateMain.java
@@ -3,23 +3,43 @@ package Hibernate.Control.Main;
 import Library.Dto.java.DTOAccount.AccountBase;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateMain {
-    public static void main(String[] args) {
+    /*private static final VarHandle MODIFIERS;
+
+    static {
+        try {
+            var lookup = MethodHandles.privateLookupIn(Field.class, MethodHandles.lookup());
+            MODIFIERS = lookup.findVarHandle(Field.class, "modifiers", int.class);
+        } catch (IllegalAccessException | NoSuchFieldException ex) {
+            throw new RuntimeException(ex);
+        }
+    }*/
+    public static void main(String[] args){
      //   Configuration configuration = new Configuration().configure();
         //configuration.addAnnotatedClass(AccountBase.class);
        // StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
 
-        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        /*var emptyElementDataField = ArrayList.class.getDeclaredField("EMPTY_ELEMENTDATA");
+        MODIFIERS.set(emptyElementDataField, emptyElementDataField.getModifiers() & ~Modifier.FINAL);*/
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+        SessionFactory factory = new MetadataSources(registry)
+                .buildMetadata().buildSessionFactory();
         Session session = factory.openSession();
         Transaction transaction = session.beginTransaction();
-        AccountBase account = new AccountBase("First", "firstpassword");
-        session.save(account);
+        AccountBase ac = new AccountBase("first", "second");
+        session.save(ac);
         transaction.commit();
         session.close();
+        factory.close();
 
     }
 
