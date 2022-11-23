56dc59fa026116a64590f2278f8beb48fb5c6762
Nikola <nikola.lazarov@smart-soft.net>
2022-11-23 21:35:23 +0200

    Implemented repository pattern

--- src/main/java/com/example/librarysoftware/LoginPage.java
+++ src/main/java/com/example/librarysoftware/LoginPage.java
@@ -1,22 +1,27 @@
 package com.example.librarysoftware;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginPage extends Application {
+    @FXML
+    private TextField user;
     @Override
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
         primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        user.getText();
     }
 
     public static void main(String args[]) {
