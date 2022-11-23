ba534e9a514ae26819cd90b93ec79bc0ae6288cf
Nikola <nikola.lazarov@smart-soft.net>
2022-11-23 22:31:19 +0200

    Added Accounts table name

--- src/main/java/Utils/TableNames.java
+++ src/main/java/Utils/TableNames.java
@@ -1,5 +1,11 @@
 package Utils;
 
 public enum TableNames {
+    Accounts("Accounts");
 
+    public final String label;
+
+    private TableNames(String label) {
+        this.label = label;
+    }
 }
