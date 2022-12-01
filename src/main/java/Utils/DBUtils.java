398e347af45ecfc971575d1524253c003f3100f6
atanas2 <atdimov71@gmail.com>
2022-12-01 12:58:09 +0200

    Added controllers for login and scene switching

---
+++ src/main/java/Utils/DBUtils.java
@@ -0,0 +1,10 @@
+package Utils;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+
+public class DBUtils {
+    public static void changeScene(ActionEvent event,String fxmlFile,String title,String username,String password){
+        Parent root = null;
+    }
+}
