128833d51b3c2135dc7b7e153df08bb71bfc159a
Nikola <nikola.lazarov@smart-soft.net>
2022-12-11 14:55:31 +0200

    Added factory class with three methods for creating library, book and author repository

---
+++ src/main/java/Hibernate/Control/Main/Repository/RepositoryFactory.java
@@ -0,0 +1,17 @@
+package Hibernate.Control.Main.Repository;
+
+import Hibernate.Control.Main.HibernateMain;
+
+public class RepositoryFactory {
+    public static LibraryRepositoryInterface CreateLibraryRepository(){
+        return new LibraryRepository(new HibernateMain());
+    }
+
+    public static BookRepository CreateBookRepository(){
+        return new BookRepository(new HibernateMain());
+    }
+
+    public static AuthorRepository CreateAuthorRepository(){
+        return new AuthorRepository(new HibernateMain());
+    }
+}
