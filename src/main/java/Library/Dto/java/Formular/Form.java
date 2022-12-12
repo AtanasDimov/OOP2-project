553d31a1e1ad428911a581a3d814ee3a4d483d34
Nikola <nikola.lazarov@smart-soft.net>
2022-12-12 09:41:05 +0200

    Added constructor, getters and setters to the form class

--- src/main/java/Library/Dto/java/Formular/Form.java
+++ src/main/java/Library/Dto/java/Formular/Form.java
@@ -3,6 +3,7 @@ package Library.Dto.java.Formular;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import java.time.LocalDate;
 import java.util.Date;
 @Entity
 public class Form {
@@ -13,5 +14,54 @@ public class Form {
     private String LastName;
     private String Username;
     private String Password;
-    private Date DateOfRegistry;
+    private LocalDate DateOfRegistry;
+
+    public Form(int id, String firstName, String lastName, String username, String password) {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Username = username;
+        Password = password;
+        DateOfRegistry = LocalDate.now();
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
 }
