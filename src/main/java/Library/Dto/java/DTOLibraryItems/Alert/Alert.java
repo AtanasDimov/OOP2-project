d242bd69c62a90a3ea4b0a0eb8c5016cbe3eef1d
Nikola <nikola.lazarov@smart-soft.net>
2022-12-12 09:46:26 +0200

    Added alert class and AlertSeverity enum

---
+++ src/main/java/Library/Dto/java/DTOLibraryItems/Alert/Alert.java
@@ -0,0 +1,40 @@
+package Library.Dto.java.DTOLibraryItems.Alert;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.time.LocalDate;
+
+@Entity
+public class Alert {
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+    private String Message;
+    private LocalDate DateOfAlert;
+    private String Severity;
+
+    public Alert(int id, String message, AlertSeverity severity) {
+        Id = id;
+        Message = message;
+        DateOfAlert = LocalDate.now();
+        Severity = severity.toString();
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+
+    public void setMessage(String message) {
+        Message = message;
+    }
+
+}
---
+++ src/main/java/Library/Dto/java/DTOLibraryItems/Alert/AlertSeverity.java
@@ -0,0 +1,7 @@
+package Library.Dto.java.DTOLibraryItems.Alert;
+
+public enum AlertSeverity {
+    NewReader,
+    ArchiveNeed,
+    OverDue
+}
