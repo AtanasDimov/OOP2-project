60aaf331d2eddb8a0205e7382217faaba21b576f
Nikola <nikola.lazarov@smart-soft.net>
2023-01-02 15:52:32 +0200

    Added try catch block to the AuthorRepository

--- src/main/java/Hibernate/Control/Main/Repository/AuthorRepository.java
+++ src/main/java/Hibernate/Control/Main/Repository/AuthorRepository.java
@@ -1,52 +1,97 @@
 package Hibernate.Control.Main.Repository;
 
+import ExceptionHandling.LibraryException;
+import ExceptionHandling.SeverityCodes;
 import Hibernate.Control.Main.HibernateMain;
 import Library.Dto.java.DTOLibraryItems.*;
+import Logger.Logger;
 import Utils.QueryGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AuthorRepository extends LibraryRepository{
+    private Logger _logger;
 
     public AuthorRepository(HibernateMain hibernateManager) {
         super(hibernateManager);
+        _logger = new Logger();
     }
 
     public Author GetEagerAuthor(int id){
-        Author result = (Author) hibernateManager.GetObject(QueryGenerator.GetLoadLazyDataAuthorQuery(id));
-        hibernateManager.CloseSession();
-        return result;
+        try{
+            Author result = (Author) hibernateManager.GetObject(QueryGenerator.GetLoadLazyDataAuthorQuery(id));
+            return result;
+        }
+        catch(Exception ex){
+            _logger.LogException(new LibraryException(ex.getMessage(), SeverityCodes.Medium));
+        }
+        finally {
+            hibernateManager.CloseSession();
+        }
+        return null;
     }
 
     public Author GetLazyDataAuthor(Author a){
-        Author result = (Author) hibernateManager.GetObject(QueryGenerator.GetLoadLazyDataAuthorQuery(a.getId()));
-        a = result;
-        hibernateManager.CloseSession();
-        return result;
+        try{
+            Author result = (Author) hibernateManager.GetObject(QueryGenerator.GetLoadLazyDataAuthorQuery(a.getId()));
+            a = result;
+            return result;
+        }
+        catch (Exception ex){
+            _logger.LogException(new LibraryException(ex.getMessage(), SeverityCodes.Medium));
+        }
+        finally {
+            hibernateManager.CloseSession();
+        }
+        return null;
     }
 
     public List<BookAuthor> GetBookAuthors(){
-        List<BookAuthor> authors = new ArrayList<>();
-        authors = (List<BookAuthor>) (Object) hibernateManager.GetListOfObject(QueryGenerator.GetBookAuthors());
-        return authors;
+        try {
+            List<BookAuthor> authors = new ArrayList<>();
+            authors = (List<BookAuthor>) (Object) hibernateManager.GetListOfObject(QueryGenerator.GetBookAuthors());
+            return authors;
+        }
+        catch(Exception ex){
+            _logger.LogException(new LibraryException(ex.getMessage(), SeverityCodes.Medium));
+        }
+        return null;
     }
 
     public List<AudioBookNarrator> GetAudioBookNarrators(){
-        List<AudioBookNarrator> authors = new ArrayList<>();
-        authors = (List<AudioBookNarrator>) (Object) hibernateManager.GetListOfObject(QueryGenerator.GetNarrators());
-        return authors;
+        try {
+            List<AudioBookNarrator> authors = new ArrayList<>();
+            authors = (List<AudioBookNarrator>) (Object) hibernateManager.GetListOfObject(QueryGenerator.GetNarrators());
+            return authors;
+        }
+        catch(Exception ex){
+            _logger.LogException(new LibraryException(ex.getMessage(), SeverityCodes.Medium));
+        }
+        return null;
     }
 
     public List<MovieDirector> GetMovieDirectors(){
-        List<MovieDirector> authors = new ArrayList<>();
-        authors = (List<MovieDirector>) (Object) hibernateManager.GetListOfObject(QueryGenerator.GetMovieDirectors());
-        return authors;
+        try {
+            List<MovieDirector> authors = new ArrayList<>();
+            authors = (List<MovieDirector>) (Object) hibernateManager.GetListOfObject(QueryGenerator.GetMovieDirectors());
+            return authors;
+        }
+        catch(Exception ex){
+            _logger.LogException(new LibraryException(ex.getMessage(), SeverityCodes.Medium));
+        }
+        return null;
     }
 
     public List<MusicArtist> GetMusicArtists(){
-        List<MusicArtist> authors = new ArrayList<>();
-        authors = (List<MusicArtist>) (Object) hibernateManager.GetListOfObject(QueryGenerator.GetMusicArtists());
-        return authors;
+        try {
+            List<MusicArtist> authors = new ArrayList<>();
+            authors = (List<MusicArtist>) (Object) hibernateManager.GetListOfObject(QueryGenerator.GetMusicArtists());
+            return authors;
+        }
+        catch (Exception ex){
+            _logger.LogException(new LibraryException(ex.getMessage(), SeverityCodes.Medium));
+        }
+        return null;
     }
 }
